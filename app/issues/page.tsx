531c1e6d41fb4406bea91dd5e32d9179f70a35b9
umut.yilmaz@phimajor.com <umut.yilmaz@phimajor.com>
2026-06-08 18:22:17 +0300

    Build a styled link component

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,6 +1,6 @@
 import { prisma } from '@/prisma/client';
-import { Button, Table } from '@radix-ui/themes';
-import Link from 'next/link';
+import Link from '../components/Link';
+import { Table } from '@radix-ui/themes';
 import IssueStatusBadge from '../components/IssueStatusBadge';
 import IssueActions from './components/IssueActions';
 
