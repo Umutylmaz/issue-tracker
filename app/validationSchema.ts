3131191b83796e41cb3ac8cde431c948fc106eb2
umut.yilmaz@phimajor.com <umut.yilmaz@phimajor.com>
2026-06-10 11:43:09 +0300

    Build an API for updating issues

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -1,6 +1,13 @@
+import { Status } from "@prisma/client";
 import { z } from "zod";
 
-export const createIssueSchema = z.object({
+export const issueSchema = z.object({
     title: z.string().min(1, 'Title is required.').max(255),
     description: z.string().min(1, 'Description is required.')
+});
+
+export const patchIssueSchema = z.object({
+    title: z.string().min(1, 'Title is required.').max(255).optional(),
+    description: z.string().min(1, 'Description is required.').optional(),
+    status: z.nativeEnum(Status).optional(),
 });
