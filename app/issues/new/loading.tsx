e4d6cccad961b4ac78498ce436caf740a65f64fa
umut.yilmaz@phimajor.com <umut.yilmaz@phimajor.com>
2026-06-10 15:29:37 +0300

    Improved the loading experience

--- app/issues/new/loading.tsx
+++ app/issues/new/loading.tsx
@@ -1,12 +1,3 @@
-import { Box, Skeleton } from '@radix-ui/themes';
+import IssueFormSkeleton from '../_components/IssueFormSkeleton';
 
-const LoadingNewIssuePage = () => {
-  return (
-    <Box className="max-w-xl ">
-      <Skeleton className="mb-5" />
-      <Skeleton height="20rem" />
-    </Box>
-  );
-};
-
-export default LoadingNewIssuePage;
+export default IssueFormSkeleton;
