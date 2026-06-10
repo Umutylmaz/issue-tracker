441ba0205f7a6996e89cfb80dc6e55c3de577507
umut.yilmaz@phimajor.com <umut.yilmaz@phimajor.com>
2026-06-10 15:05:06 +0300

    Fix caching issues

--- app/validationSchema.ts
+++ app/validationSchema.ts
@@ -9,5 +9,6 @@ export const issueSchema = z.object({
 export const patchIssueSchema = z.object({
     title: z.string().min(1, 'Title is required.').max(255).optional(),
     description: z.string().min(1, 'Description is required.').optional(),
+    //not in the application rn, only valid for postman
     status: z.nativeEnum(Status).optional(),
 });
