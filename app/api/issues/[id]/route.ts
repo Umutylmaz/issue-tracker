015d966b3e83947d13fcdc5f949dfed98dc2fcad
umut.yilmaz@phimajor.com <umut.yilmaz@phimajor.com>
2026-06-10 20:40:50 +0300

    Build API for deleting issues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -29,3 +29,21 @@ export async function PATCH(request: NextRequest, { params }: { params: Promise<
     return NextResponse.json(updatedIssue)
 
 }
+
+export async function DELETE(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+
+    const paramId = (await params).id;
+
+    const issue = await prisma.issue.findUnique({
+        where: { id: parseInt(paramId) }
+    })
+
+    if (!issue) return NextResponse.json({ error: 'Invalid issue' }, { status: 404 })
+
+    await prisma.issue.delete({
+        where: { id: parseInt(paramId) }
+    })
+
+    return NextResponse.json({})
+
+}
