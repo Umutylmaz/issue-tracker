a132720f4f539cd0534940ade7ec1249c1434410
umut.yilmaz@phimajor.com <umut.yilmaz@phimajor.com>
2026-06-13 15:11:23 +0300

    Secure the application

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,8 +1,13 @@
+import authOptions from "@/app/auth/authOptions";
 import { patchIssueSchema } from "@/app/validationSchema";
 import { prisma } from "@/prisma/client";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    const session = await getServerSession(authOptions);
+    if (!session)
+        return NextResponse.json({}, { status: 401 })
 
     const body = await request.json();
 
@@ -31,6 +36,10 @@ export async function PATCH(request: NextRequest, { params }: { params: Promise<
 }
 
 export async function DELETE(request: NextRequest, { params }: { params: Promise<{ id: string }> }) {
+    const session = await getServerSession(authOptions);
+    if (!session)
+        return NextResponse.json({}, { status: 401 })
+
     const paramId = (await params).id;
 
     const issue = await prisma.issue.findUnique({
