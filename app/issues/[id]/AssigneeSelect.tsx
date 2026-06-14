67d4644624d56eadd739c38357f2237ce66267a9
umut.yilmaz@phimajor.com <umut.yilmaz@phimajor.com>
2026-06-14 12:10:45 +0300

    Populate the assignee select component

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,16 +1,31 @@
 'use client';
 
 import { Select } from '@radix-ui/themes';
+import axios from 'axios';
+import { User } from 'next-auth';
+import { useEffect, useState } from 'react';
 
 const AssigneeSelect = () => {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    const fetchUsers = async () => {
+      const { data } = await axios.get<User[]>('/api/users');
+      setUsers(data);
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <Select.Root defaultValue="apple">
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          <Select.Item value="1">Orange</Select.Item>
-          <Select.Item value="2">Apple</Select.Item>
+          {users.map((user) => (
+            <Select.Item value={user.id} key={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
