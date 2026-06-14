2adb10102dde602bea0f3c9eb97dbe4df5551499
umut.yilmaz@phimajor.com <umut.yilmaz@phimajor.com>
2026-06-14 12:54:46 +0300

    Fetch data with React Query

--- app/issues/[id]/AssigneeSelect.tsx
+++ app/issues/[id]/AssigneeSelect.tsx
@@ -1,27 +1,33 @@
 'use client';
 
-import { Select } from '@radix-ui/themes';
+import { Select, Skeleton } from '@radix-ui/themes';
+import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import { User } from 'next-auth';
-import { useEffect, useState } from 'react';
 
 const AssigneeSelect = () => {
-  const [users, setUsers] = useState<User[]>([]);
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const { data } = await axios.get<User[]>('/api/users');
-      setUsers(data);
-    };
-    fetchUsers();
-  }, []);
+  const {
+    data: users,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ['users'],
+    queryFn: () => axios.get<User[]>('/api/users').then((res) => res.data),
+    staleTime: 60 * 1000, //60s
+    retry: 3,
+  });
+
+  if (isLoading) return <Skeleton height="2rem" />;
+
+  if (error) return null;
 
   return (
-    <Select.Root defaultValue="apple">
+    <Select.Root>
       <Select.Trigger placeholder="Assign..." />
       <Select.Content>
         <Select.Group>
           <Select.Label>Suggestions</Select.Label>
-          {users.map((user) => (
+          {users?.map((user) => (
             <Select.Item value={user.id} key={user.id}>
               {user.name}
             </Select.Item>
