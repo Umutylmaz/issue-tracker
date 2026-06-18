2692d6bb742a6380848d05075c457d7701e597b0
umut.yilmaz@phimajor.com <umut.yilmaz@phimajor.com>
2026-06-18 19:50:40 +0300

    Build latest issue component

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,5 @@
-import Pagination from './components/Pagination';
+import LatestIssues from '@/LatestIssues';
 
-export default async function Home({
-  searchParams,
-}: {
-  searchParams: Promise<{ page: string }>;
-}) {
-  const resolvedParams = await searchParams;
-  return (
-    <Pagination
-      itemCount={20}
-      pageSize={1}
-      currentPage={parseInt(resolvedParams.page) || 1}
-    ></Pagination>
-  );
+export default function Home() {
+  return <LatestIssues />;
 }
