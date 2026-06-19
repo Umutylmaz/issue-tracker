7c0718b2c454b2f0e4bffc567093b908808fa738
umut.yilmaz@phimajor.com <umut.yilmaz@phimajor.com>
2026-06-20 00:17:19 +0300

    Add metadata

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -42,4 +42,15 @@ const IssueDetailPage = async ({ params }: Props) => {
   );
 };
 
+export async function generateMetadata({ params }: Props) {
+  const resolvedParams = await params;
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(resolvedParams.id) },
+  });
+  return {
+    title: issue?.title,
+    description: 'Details of issue ' + issue?.id,
+  };
+}
+
 export default IssueDetailPage;
--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -4,6 +4,7 @@ import Pagination from '../components/Pagination';
 import IssueActions from './_components/IssueActions';
 import IssueTable, { columnNames, IssueQuery } from './_components/IssueTable';
 import { Flex } from '@radix-ui/themes';
+import { Metadata } from 'next';
 
 interface Props {
   searchParams: Promise<IssueQuery>;
@@ -53,3 +54,8 @@ export const dynamic = 'force-dynamic';
 // export const revalidate = 0;
 
 export default IssuesPage;
+
+export const metadata: Metadata = {
+  title: 'Issue Tracker - Issue List',
+  description: 'View all project issues.',
+};
